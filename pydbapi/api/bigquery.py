8e5d095c0c1fa6bea644b0c5762b844dd2a76080
0829 <a0829@0829deMacBook-Pro.local>
2025-07-28 19:04:35 +0800

    api/bigquery.py

--- pydbapi/api/bigquery.py
+++ pydbapi/api/bigquery.py
@@ -1,189 +1,164 @@
-# -*- coding: utf-8 -*-
-# @Author: longfengpili
-# @Date:   2023-06-02 15:27:41
-# @Last Modified by:   longfengpili
-# @Last Modified time: 2024-11-20 18:14:45
+# @Author: chunyang.xu
+# @Email:  398745129@qq.com
+# @Date:   2020-06-12 12:00:28
+# @Last Modified time: 2021-04-13 20:06:33
 # @github: https://github.com/longfengpili
 
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
 
-import re
-import threading
-from datetime import date
+import os
 
-import pymysql
+from google.cloud import bigquery
+from google.cloud.bigquery import dbapi
 
-from pydbapi.db import DBMixin, DBFileExec
-from pydbapi.model import ColumnModel, ColumnsModel
+from pydbapi.db import DBCommon, DBFileExec
 from pydbapi.sql import SqlCompile
 from pydbapi.conf import AUTO_RULES
 
 
 import logging
-mysqllogger = logging.getLogger(__name__)
+import logging.config
+from pydbapi.conf import LOGGING_CONFIG
+logging.config.dictConfig(LOGGING_CONFIG)
+bigquerylog = logging.getLogger('bigquery')
 
 
-class SqlMysqlCompile(SqlCompile):
+class SqlBigqueryCompile(SqlCompile):
     '''[summary]
 
     [description]
-        构造mysql sql
+        构造bigquery sql
     Extends:
         SqlCompile
     '''
 
     def __init__(self, tablename):
-        super(SqlMysqlCompile, self).__init__(tablename)
+        super(SqlBigqueryCompile, self).__init__(tablename)
 
-    def create_indexes(self, columns, indexes, index_part=128, ismultiple=True):
-        if indexes and not isinstance(indexes, list):
-            raise TypeError(f"indexes must be a list, but got {indexes} !")
-        _indexes = []
-        for index in indexes:
-            column = columns.get_column_by_name(index)
-            coltype = column.coltype
-            varlength = re.search('varchar\((\d+)\)', coltype)
-            varlength = int(varlength.group(1)) if varlength else 128
-            index_part = varlength if varlength < index_part else index_part
-            index = f"{index}({index_part})" if coltype.startswith('varchar') else index
-            _indexes.append(index)
-
-        if ismultiple:
-            indexes = ', '.join(_indexes)
-            indexes = f"index multiple_index({indexes})"
-        else:
-            indexes = [f"index {_index}_index({_index})" for _index in indexes]
-            indexes = ',\n'.join(indexes)
-
-        return indexes
-
-    def create_partition(self, partition):
-        coltype = partition.coltype
-        if coltype != 'date':
-            raise TypeError(f"{partition} only support date type !")
-        partition = f"partition by hash (to_days({partition.newname}))"
-        return partition
-
-    def create(self, columns, indexes, index_part=128, ismultiple_index=True, 
-               partition=None, distribution=None, isdoris=False):
-        'mysql 暂不考虑索引'
+    def create(self, columns, indexes):
         sql = self.create_nonindex(columns)
-
         if indexes and not isinstance(indexes, list):
-            raise TypeError(f"indexes must be a list, but got {indexes} !")
-
+            raise TypeError(f"indexes must be a list !")
         if indexes:
-            indexes = self.create_indexes(columns, indexes, index_part=index_part, ismultiple=ismultiple_index)
-            sql = sql - ')' + f",{indexes})"
-
-        if partition:
-            partition = columns.get_column_by_name(partition)
-            partition = self.create_partition(partition)
-            sql += partition
-
-        if isdoris:
-            distributed_col = distribution or columns.index(0).newname
-            distributed = f"distributed by hash({distributed_col})"
-            sql += distributed
-
+            indexes = ','.join(indexes)
+            sql = f"{sql.replace(r');', '')},index({indexes}));"
         return sql
 
-    def dumpsql(self, columns, dumpfile, fromtable=None, condition=None):
-        selectsql = self.select_base(columns, fromtable=fromtable, condition=condition)
-        intosql = f'into outfile "{dumpfile}" fields terminated by ",";'
-        dumpsql = selectsql + intosql
-        return dumpsql
-
-    def loadsql(self, columns, loadfile, intotable=None, fieldterminated=','):
-        intotable = intotable or self.tablename
-        loadsql = f'''load data infile "{loadfile}" into table {intotable}
-                      fields terminated by "{fieldterminated}" ({columns.select_cols});'''
-        return loadsql
+    def add_columns(self, col_name, col_type):
+        col_type = f"{col_type}(32)" if col_type == 'varchar' else col_type
+        sql = f'alter table {self.tablename} add column {col_name} {col_type};'
+        return sql
 
 
-class MysqlDB(DBMixin, DBFileExec):
-    _instance_lock = threading.Lock()
+class BigqueryDB(DBCommon, DBFileExec):
 
-    def __init__(self, host, user, password, database, port=3306, charset="utf8", safe_rule=True, isdoris=False):
-        self.host = host
-        self.port = port
-        self.user = user
-        self.password = password
-        self.database = database
-        self.charset = charset
-        self.isdoris = isdoris
-        super(MysqlDB, self).__init__()
-        self.auto_rules = AUTO_RULES if safe_rule else None
-        self.dbtype = 'mysql'
+    def __init__(self, service_account):
+        self.service_account = self.__check_service_account(service_account)
+        super(BigqueryDB, self).__init__()
+        self.auto_rules = AUTO_RULES
 
-    # def __new__(cls, *args, **kwargs):
-    #     if not hasattr(MysqlDB, '_instance'):
-    #         with MysqlDB._instance_lock:
-    #             if not hasattr(MysqlDB, '_instance'):
-    #                 MysqlDB._instance = super().__new__(cls)
-
-    #     return MysqlDB._instance
-
-    @classmethod
-    def get_instance(cls, *args, **kwargs):
-        # mysqllogger.info(MysqlDB._instance_lock)
-        if not hasattr(MysqlDB, '_instance'):
-            # mysqllogger.info(MysqlDB._instance_lock)
-            with MysqlDB._instance_lock:
-                if not hasattr(MysqlDB, '_instance'):
-                    MysqlDB._instance = cls(*args, **kwargs)
-
-        return MysqlDB._instance
+    def __check_service_account(self, service_account):
+        if not os.path.exists(service_account):
+            url = 'https://console.cloud.google.com/apis/credentials/serviceaccountkey'
+            raise Exception(f"请前往{url}申请并下载")
+        return service_account
 
     def get_conn(self):
-        if not hasattr(MysqlDB, '_conn'):
-            with MysqlDB._instance_lock:
-                if not hasattr(MysqlDB, '_conn'):
-                    conn = pymysql.connect(database=self.database, user=self.user, password=self.password,
-                                           host=self.host, port=self.port, charset=self.charset)
-                    mysqllogger.info(f'connect {self.__class__.__name__}({self.user}@{self.host}:{self.port}/{self.database})')
-                    MysqlDB._conn = conn
-        return MysqlDB._conn
+        client = bigquery.Client.from_service_account_json(self.service_account)
+        conn = dbapi.connect(client)
+        if not conn:
+            self.get_conn()
+        return conn
 
-    def cur_columns(self, cursor):
-        desc = cursor.description
-        columns = ColumnsModel(*tuple(map(lambda x: ColumnModel(x[0], 'varchar'), desc))) if desc else None
-        return columns
-
-    def create(self, tablename, columns, indexes=None, index_part=128, ismultiple_index=True,
-               partition=None, distribution=None, verbose=0):
+    def create(self, tablename, columns, indexes=None):
         # tablename = f"{self.database}.{tablename}"
-        sqlcompile = SqlMysqlCompile(tablename)
-        sql_for_create = sqlcompile.create(columns, indexes, index_part=index_part,
-                                           ismultiple_index=ismultiple_index, partition=partition,
-                                           distribution=distribution, isdoris=self.isdoris)
-        cursor, action, result = self.execute(sql_for_create, verbose=verbose)
-        return cursor, action, result
+        sqlcompile = SqlBigqueryCompile(tablename)
+        sql_for_create = sqlcompile.create(columns, indexes)
+        rows, action, result = self.execute(sql_for_create)
+        return rows, action, result
 
-    def dumpdata(self, tablename, columns, dumpfile, condition=None, verbose=0):
-        sqlcompile = SqlMysqlCompile(tablename)
-        sql_for_dump = sqlcompile.dumpsql(columns, dumpfile, condition=condition)
-        cursor, action, result = self.execute(sql_for_dump, verbose=verbose)
-        mysqllogger.info(f"【{action}】{tablename} dumpdata {cursor.rowcount} rows succeed, outfile: {dumpfile} !")
-        return cursor, action, result
+    def select(self, tablename, columns, condition=None):
+        '''[summary]
 
-    def loaddata(self, tablename, columns, loadfile, fieldterminated=',', verbose=0):
-        sqlcompile = SqlMysqlCompile(tablename)
-        sql_for_load = sqlcompile.loadsql(columns, loadfile, fieldterminated=fieldterminated)
-        cursor, action, result = self.execute(sql_for_load, verbose=verbose)
-        mysqllogger.info(f"【{action}】{tablename} loaddata {cursor.rowcount} rows succeed, loadfile: {loadfile} !")
-        return cursor, action, result
+        [description]
+            查询数据，暂时不考虑join形式。如果是join形式请使用原始sql查询。
+        Arguments:
+            tablename {[str]} -- [表名]
+            columns {[dict]} -- [列的信息]
+            {'id_rename': {'order': 1, 'source_col':'datas', 'source_type': '', 'func': 'min', 'source_name': 'id'}, ……}
+                # order: 用于排序
+                # source_col: 原始数据列名 用于解析
+                # source_type: 原始数据类型 用于解析
+                # source_name: 解析的KEY或者原始数据的列名
+                # func: 后续处理的函数
 
-    def alter_tablecol(self, tablename: str, colname: str, newname: str = None, newtype: str = None, 
-                       indexes: list = None, index_part: int = 128, ismultiple_index: bool = True,
-                       partition: str = None, distribution: str = None, conditions: list[str] = None, 
-                       verbose: int = 0):
-        alter_columns = self.alter_column(tablename, colname, newname, newtype)
+        Keyword Arguments:
+            condition {[str]} -- [查询条件] (default: {None})
 
-        if alter_columns:
-            # create tmp table
-            mtablename = f"{tablename}_tmp"
-            self.create(mtablename, alter_columns, indexes=indexes, index_part=index_part, ismultiple_index=ismultiple_index,
-                        partition=partition, distribution=distribution, verbose=verbose)
+        Returns:
+            [type] -- [description]
+        '''
 
-            # alter table
-            self.alter_tablecol_base(tablename, mtablename, alter_columns, conditions=conditions, verbose=verbose)
+        def deal_columns(columns):
+            '''[summary]
+
+            [description]
+                处理columns
+            Arguments:
+                columns {[dict]} -- [原始dict]
+                {'id_rename': {'order': 1, 'source_col':'datas', 'source_type': '', 'func': 'min', 'source_name': 'id'}, ……}
+            Returns:
+                [dict] -- [构造columns]
+                {'id_rename': {'source':'id', 'func': 'min', 'order': 1}, ……}
+            '''
+            columns_dealed = {}
+            if not isinstance(columns, dict):
+                raise TypeError(f"columns must be a dict !")
+
+            for col, info in columns.items():
+                if not isinstance(info, dict):
+                    raise TypeError(f"【({col}){info}】info must be a dict !")
+
+                tmp = {}
+                source_col = info.get('source_col')
+                source_type = info.get('source_type', 'json')  # 默认json处理
+                source_name = info.get('source_name', col)  # 不存在就是用命名列
+                func = info.get('func')
+                order = info.get('order')
+
+                if func:
+                    tmp['func'] = func
+                if order:
+                    tmp['order'] = order
+                if source_col and source_type == 'json':
+                    source_name = f"JSON_EXTRACT({source_col}, '$.{source_name}')"
+                tmp['source'] = source_name
+
+                columns_dealed[col] = tmp
+            return columns_dealed
+
+        columns = deal_columns(columns)
+        sqlcompile = SqlBigqueryCompile(tablename)
+        sql_for_select = sqlcompile.select_base(columns, condition)
+        rows, action, result = self.execute(sql_for_select)
+        return rows, action, result
+
+    def add_columns(self, tablename, columns):
+        old_columns = self.get_columns(tablename)
+        old_columns = set(old_columns)
+        new_columns = set(columns)
+        # bigquerylog.info(f'{old_columns}, {new_columns}')
+
+        if old_columns == new_columns:
+            bigquerylog.info(f'【{tablename}】columns not changed !')
+        if old_columns - new_columns:
+            raise Exception(f"【{tablename}】columns【{old_columns - new_columns}】 not set, should exists !")
+        if new_columns - old_columns:
+            sqlcompile = SqlBigqueryCompile(tablename)
+            add_columns = new_columns - old_columns
+            for col_name in add_columns:
+                col_type = columns.get(col_name)
+                sql = sqlcompile.add_columns(col_name, col_type)
+                self.execute(sql)
+            bigquerylog.info(f'【{tablename}】add columns succeed !【{new_columns - old_columns}】')
